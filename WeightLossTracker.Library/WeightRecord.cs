03a089399cc3460ad5ab0ea9d3ac7dc2e5de481f
Michal Brejnakowski <mbrejnakowski24@gmail.com>
2020-10-13 20:58:14 +0200

    Add project files.

---
+++ WeightLossTracker.Library/WeightRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightLossTracker.Library
+{
+    public class WeightRecord
+    {
+        public double Weight { get; set; }
+        DateTime Date { get; set; }
+    }
+}
---
+++ WeightLossTracker.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeightLossTracker.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeightLossTracker.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4299a027-af69-40d2-a1aa-f279b6cf6c5e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
